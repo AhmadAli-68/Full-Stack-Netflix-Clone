cdf53bac71dd93fcb8f27ca0e6cf67bbe7f4df63
Ahmad Ali <ahmad.ali619x@gmail.com>
2026-08-22 15:36:50 +0500

    Implement the login page

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,26 +1,38 @@
 import './Login.css'
 import logo from '../../assets/logo.png'
+import { useState } from 'react'
 
 const Login = () => {
+  const [signState, setSignState] = useState('Sign In')
+
   return (
     <div className='login'>
       <img src={logo} className='login-logo' alt='logo' />
       <div className='login-form'>
-        <h1>Sign In</h1>
+        <h1>{signState}</h1>
 
         <form>
-          <input type='text' placeholder='Your Name' />
+          {signState === 'Sign Up' ? <input type='text' placeholder='Your Name' /> : <></>}
           <input type='email' placeholder='Email' />
           <input type='password' placeholder='Password' />
-          <button>Sign In</button>
+          <button>{signState}</button>
 
           <div className='form-help'>
             <div className='remember'>
               <input type='checkbox' />
-              <label htmlFor=''></label>
+              <label htmlFor=''>Remember me</label>
             </div>
+
+            <p>Need Help?</p>
           </div>
         </form>
+
+        <div className='form-switch'>
+          {signState === 'Sign In'
+            ? <p>New to Netflix? <span>Sign up Now</span></p>
+            : <p>Already have an Account? <span>Sign in Now</span></p>
+          }
+        </div>
       </div>
     </div>
   )
